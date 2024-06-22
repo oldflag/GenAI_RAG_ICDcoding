683881b9fa7a5bfe790b86bcef95bd29391e2570
oldflag <oldflag@gmail.com>
2024-06-21 17:05:55 -0700

    utility function

--- utils.py
+++ utils.py
@@ -21,8 +21,37 @@ def parse_icd10_description(icdfile):
 
     return df
 
+def filter_symptom_from_icd(symptomfile, icdfile):
+    # Load the CSV file
+    symptom2disease_df = pd.read_csv(symptomfile)
+
+    # Load the ICD codes file
+    icd_df = parse_icd10_description(icdfile)
+
+    # Combine short and long descriptions into a single list
+    icd_descriptions = icd_df['ShortDescription'].str.lower().tolist() + icd_df['LongDescription'].str.lower().tolist()
+
+    # Filter rows where label is related to ICD descriptions
+    def is_related(label, descriptions):
+        for description in descriptions:
+            if label.lower() in description :
+                return True
+        return False
+
+    filtered_df = symptom2disease_df[symptom2disease_df['label'].apply(lambda x: is_related(x, icd_descriptions))]
+
+    # Save the filtered rows to a new CSV file
+    # filtered_df.to_csv(symptomfile+'.filtered.csv', index=False)
+    return filtered_df
+
 if __name__ == "__main__":
     icdfile_path = './icd10cm_order_2024_top100.txt'  # only first 100 rows for testing
-    icd10_df = parse_icd10_description(icdfile_path)
-    print(icd10_df.head())
+
+    # icd10_df = parse_icd10_description(icdfile_path)
+    # print(icd10_df.head())
+
+    symptom_path = './Symptom2Disease.csv'  # only first 100 rows for testing
+    filtered_symptom_df=filter_symptom_from_icd(symptom_path, icdfile_path)
+    print(filtered_symptom_df.head())
+    filtered_symptom_df.to_csv(symptom_path+'.filtered.csv', index=False)
 
